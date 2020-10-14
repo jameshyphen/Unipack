251cff670972d87358876f5bb0d65a3f5d59c9ac
Dzhem <dzhem.aptula@gmail.com>
2020-10-14 14:34:52 +0200

    fix imports

--- Unipack/Startup.cs
+++ Unipack/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
