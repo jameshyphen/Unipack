ad0bafcf9e9fa85a4829b8027aba0c02b63087ce
Luna De Vuyst <luna.devuyst@student.hogent.be>
2020-10-14 14:51:13 +0200

    create model ItemCategory

---
+++ Unipack/Models/ItemCategory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unipack.Models
+{
+    public class ItemCategory
+    {
+        public int ItemCategoryID { get; set; }
+
+        public string Name { get; set; }
+        public List<Item> Items { get; set; }
+        public DateTime AddedOn { get; set; }
+
+        public ItemCategory(string name )
+        {
+            Name = name;
+            AddedOn = DateTime.Now;
+        }
+
+        public ItemCategory()
+        {
+        }
+
+
+
+    }
+}
