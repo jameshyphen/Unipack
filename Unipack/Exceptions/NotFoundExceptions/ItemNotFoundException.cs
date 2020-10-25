daba5cc4fa3431092cbc18f8c88c0e33a02dce42
Dzhem <dzhem.aptula@gmail.com>
2020-10-25 19:54:38 +0100

    Refactor service with exceptions and remove vars, bad practice

---
+++ Unipack/Exceptions/NotFoundExceptions/ItemNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+
+namespace Unipack.Exceptions
+{
+    public class ItemNotFoundException : Exception
+    {
+        public ItemNotFoundException(int id)
+            : base($"The requested item with the following ID={id} has not been found")
+        {
+        }
+    }
+}
---
+++ Unipack/Exceptions/NotFoundExceptions/VacationListNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unipack.Exceptions
+{
+    public class VacationListNotFoundException : Exception
+    {
+        public VacationListNotFoundException(int id)
+            : base($"The requested vacation list with the following ID={id} has not been found")
+        {
+        }
+    }
+}
