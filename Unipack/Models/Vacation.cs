5c9a381fd9654a57c241d6820d8acaa2b51e59a1
Dzhem <dzhem.aptula@gmail.com>
2020-10-25 16:20:44 +0100

    Vacation + Dto

---
+++ Unipack/Models/Vacation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unipack.Models
+{
+    public class Vacation
+    {
+        public string Name { get; set; }
+        public ICollection<VacationLocation> Locations { get; set; }
+        public ICollection<VacationList> VacationLists { get; set; }
+        public DateTime DateDeparture { get; set; }
+        public DateTime DateReturn { get; set; }
+        public DateTime AddedOn { get; set; }
+
+        public Vacation()
+        {
+            Locations = new HashSet<VacationLocation>();
+        }
+    }
+}
